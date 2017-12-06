08d032bbf8214d484260231d9d2633726d488ff3
oliverpople <oliverpople@gmail.com>
2017-12-06 11:22:31 +0000

    Initial Commit

---
+++ message_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  message_app
+//
+//  Created by Oliver Pople on 06/12/2017.
+//  Copyright © 2017 Oliver Pople. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
